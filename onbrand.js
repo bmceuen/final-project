300780e68d4d5eed4d0daa1ab492505c736ce550
aap-ksm <ashley.panlasigui@kellogg.northwestern.edu>
2021-03-01 10:30:25 -0600

    merged bm sunday 9am changes

--- onbrand.js
+++ onbrand.js
@@ -1,16 +1,26 @@
 let db = firebase.firestore()
 firebase.auth().onAuthStateChanged(async function(user)
 {
+    
     if (user)
     {
 
-        let docRef = await db.collection('answers').doc(user.uid).get()
+        // let querySnapshot = await db.collect('answers').where(`userId`, '==', user.uid).get()
+        // let answersReturn = querySnapshot.docs
+        // for(let t=0;t<answersReturn.length;t++)
+        // {
+        //     let item = answersReturn[i].data()
+        //     let category = item.
+        // }
+
+        let category = 'denim'
+
+        let docRef = await db.collection('answers').doc(`${user.uid}-${category}`).get()
         let result = docRef.data()
-        // let finalResult = result.userId
-        // console.log(finalResult)
+
 
         // let loginDiv = document.querySelector('.sign-in-or-sign-out')
-        // loginDiv.classList.add('border-2', 'text-black', 'text-center')
+        // loginDiv.classList.add('border-2', 'text-white', 'text-center', 'mx-16', 'mr-8')
         // loginDiv.innerHTML = `You Are Currently Logged In As:<br>${user.email}<br><br>
         // <button class="sign-out-button bg-tan text-bold text-storm px-4 py-2 rounded-xl">Sign-Out</button>`
 
@@ -26,10 +36,21 @@ firebase.auth().onAuthStateChanged(async function(user)
             document.location.href = 'index.html'
         })
         
+        
 
         let printProducts = async function()
         {
-            let productList = await db.collection('products').get()
+            
+        }
+
+        if(result)
+        {
+            let findBrand = await db.collection('answers').doc(`${user.uid}-${category}`).get()
+            let brandMatch = findBrand.data()
+            let brandMatchName = brandMatch.denim_answer
+            console.log(brandMatchName)
+
+            let productList = await db.collection('products').where('brand', '==',brandMatchName).get()
             // console.log(productList.size)
             let products = productList.docs
             for(let i=0; i<products.length;i++)
@@ -41,45 +62,63 @@ firebase.auth().onAuthStateChanged(async function(user)
                 let productURL = productData.url
                 let productPrice = productData.price
                 let brand = productData.brand
+                let productID = productData.productID
             
             document.querySelector('.products').insertAdjacentHTML('beforeend', `
-            <div class="product-grid w-1/5 p-4">
-                <a href="${productURL}">
-                    <img src="${productImage}" class="border-2 border-black hover:opacity-80">
+            <div class="product-grid">
+            <div class="product-${productID} p-4">
+                <a href="${productURL}" target="popup" onclick="window.open('${productURL}','name','width=1400, height=1000')">
+                    <img src="${productImage}" class="product-image border-2 border-black">
                 </a>
+                <button class="buynow hidden">BUY NOW</button>
+                <button class="btn hidden">👍</button>
+                <button class="btn2 hidden">👎</button>
                 <div class="mt-2 justify-center">
                     <p class="text-center text-2xl text-bold text-black">${brand}</p>
                 </div>
                 <div class="text-black text-center text-xl text-bold">${productName}</div>
                 <div class="text-black text-center text-2xl text-bold">${productPrice}</div>
             </div>
-
+            </div>
             `)
+            
+           document.querySelector(`.product-${productID}`).addEventListener('mouseover', function (event)
+           {
+               event.preventDefault()
+               document.querySelector(`.product-${productID} .buynow`).classList.remove('hidden')
+               document.querySelector(`.product-${productID} .btn`).classList.remove('hidden')
+               document.querySelector(`.product-${productID} .btn2`).classList.remove('hidden')
+               document.querySelector(`.product-${productID} .product-image`).classList.add('opacity-60')
+           })
+            
+           document.querySelector(`.product-${productID}`).addEventListener('mouseout', function (event)
+           {
+               event.preventDefault()
+               document.querySelector(`.product-${productID} .buynow`).classList.add('hidden')
+               document.querySelector(`.product-${productID} .btn`).classList.add('hidden')
+               document.querySelector(`.product-${productID} .btn2`).classList.add('hidden')
+               document.querySelector(`.product-${productID} .product-image`).classList.remove('opacity-60')
+           })
+           
+        
             }
-        }
-
-        if(result)
-        {
-           printProducts()
+           
         }
         else
         {
     
-
+        
         document.querySelector('.quiz').insertAdjacentHTML('beforeend', `
         <div class="border-2 border-white m-8">
         <form class="quiz text-center text-white">
         
-            <p class="text-white">What is your favorite denim brand?</p>
+            <p class="text-white">What is your favorite ${category} brand?</p>
             <input type="radio" id="levi's" name="denim" value="Levi's">
             <label for="Levi's">Levi's</label><br>
-
             <input type="radio" id="Gap" name="denim" value="Gap">
             <label for="Gap">Gap</label><br>
-
             <input type="radio" id="Lucky" name="denim" value="Lucky">
             <label for="Lucky">Lucky</label><br>
-
             <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-xl">Submit</button>
         </form>
         </div>
@@ -88,9 +127,9 @@ firebase.auth().onAuthStateChanged(async function(user)
         document.querySelector('.quiz').addEventListener('submit', async function(event){
 
             event.preventDefault()
-            let denimBrandAnswer = document.querySelector('input[name="denim"]:checked').value
+            let denimBrandAnswer = document.querySelector(`input[name="${category}"]:checked`).value
             console.log(denimBrandAnswer)
-            db.collection('answers').doc(user.uid).set({
+            db.collection('answers').doc(`${user.uid}-${category}`).set({
                 userId: user.uid,
                 denim_answer: denimBrandAnswer,
             })
@@ -99,6 +138,8 @@ firebase.auth().onAuthStateChanged(async function(user)
             
             
         })
+
+        
     }
     
         
@@ -113,6 +154,7 @@ firebase.auth().onAuthStateChanged(async function(user)
         // console.log(user.uid)
         
         console.log('user is signed in')
+        
        
     }
     
